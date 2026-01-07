970786cd062fecb1f52751211dd49fe75c67f5c6
soufyane <s.yassine@siliconesignal.com>
2026-01-07 16:46:05 +0000

    updated vpc cidr in locals

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,11 +4,8 @@ locals {
   #safety: we use either dev or prod
   allowed_envs = ["dev", "prod"]
   is_valid_env = contains(local.allowed_envs, local.env)
-
-  vpc_cidr = {
-    dev = "10.0.0.0/16"
-    prod = "10.1.0.0/16"
-  }
+  selected_vpc_cidr = local.env == "prod" ? "10.1.0.0/16" : "10.0.0.0/16"
+  
 
   tags = {
     Environement = local.env
