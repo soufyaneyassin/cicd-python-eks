6ab8a94cbb51420a17ed482d26a8e314dd85cb0a
soufyane <s.yassine@siliconesignal.com>
2026-01-14 16:06:21 +0000

    adding eks node group

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,14 +99,13 @@ resource "aws_route_table_association" "private_association" {
 
 # define the eks iam role
 resource "aws_iam_role" "eks_cluster_role" {
-       name = "eks-cluster-role"
+       name = "eks-cluster-role-${terraform.workspace}"
        assume_role_policy = jsonencode({
               Version = "2012-10-17"
               Statement = [
               {
                      Action = [
                      "sts:AssumeRole",
-                     "sts:TagSession"
                      ]
                      Effect = "Allow"
                      Principal = {
@@ -124,16 +123,64 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 
 # define the eks cluster 
 resource "aws_eks_cluster" "main_eks_cluster" {
-       name = "main-eks-cluster"
+       name = "main-eks-cluster-${terraform.workspace}"
        role_arn = aws_iam_role.eks_cluster_role.arn
       
        vpc_config {
-              subnet_ids = [
+              subnet_ids = concat(
                      aws_subnet.public_subnet[*].id,
                      aws_subnet.private_subnet[*].id
-              ]
+              )
        }
        depends_on = [
               aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
        ]
+}
+
+# define the iam role for the worker nodes managed groupe 
+resource "aws_iam_role" "eks_node_group_role" {
+       name = "eks-node_group-role-${terraform.workspace}"
+       assume_role_policy = jsonencode({
+                     Statement = [{
+                     Action = "sts:AssumeRole"
+                     Effect = "Allow"
+                     Principal = {
+                            Service = "ec2.amazonaws.com"
+                     }
+                     }]
+                     Version = "2012-10-17"
+                     })
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-group-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+# define the eks node group
+resource "aws_eks_node_group" "eks-node-group" {
+          cluster_name = aws_eks_cluster.main_eks_cluster.name
+          node_group_name = "eks-node-group-${terraform.workspace}"
+          node_role_arn = aws_iam_role.eks_node_group_role.arn
+          subnet_ids = aws_subnet.private_subnet[*].id
+          scaling_config {
+              desired_size = 2
+              max_size = 3
+              min_size = 2
+          }
+          depends_on = [
+              aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
+              aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
+              aws_iam_role_policy_attachment.node-group-AmazonEC2ContainerRegistryReadOnly,
+              ]
 }
